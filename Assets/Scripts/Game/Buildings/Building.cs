8d3d41ed0b319627865d7fafc7c9bcac4b332afa
Felix Nieuwenhuijsen <6042715@mborijnland.nl>
2026-03-16 11:21:33 +0100

    Feat: Added resource gathering for productive buildings (only the mine for now) (#4 #10))

--- Assets/Scripts/Game/Buildings/Building.cs
+++ Assets/Scripts/Game/Buildings/Building.cs
@@ -4,32 +4,32 @@ using UnityEngine;
 public class BuildingScript : MonoBehaviour
 {
     public Building thisBuilding;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     IEnumerator Start()
     {
         yield return new WaitForSeconds(0.1f);
-        //add amount of residents when residential building is built
-        Building.Types type = thisBuilding.type;
 
-        if(type == Building.Types.Residential)
+        if (thisBuilding.type == Building.Types.Residential)
         {
             GameManager.Instance.stats.MaxResidents += thisBuilding.personCapacity;
         }
 
-        StartCoroutine(BuildingTicker());
-    }
-
-    private IEnumerator BuildingTicker()
-    {
-        while (true)
+        if (thisBuilding.type == Building.Types.Productive)
         {
-            yield return new WaitForSeconds(1);
+            foreach (MaterialProduce p in thisBuilding.materialsProduce)
+            {
+                StartCoroutine(ProduceMaterial(p));
+            }
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator ProduceMaterial(MaterialProduce produce)
     {
-        
+        while (true)
+        {
+            yield return new WaitForSeconds(produce.delay);
+
+            Player.Instance.AddMaterial(produce.material.name, produce.amount);
+        }
     }
 }
